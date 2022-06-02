696ec4c8c44b4ee6a44d0b20b0618e4b4a4e678b
Onion-Bro <Lucas.Lindgren@elev.ga.lbs.se>
2022-06-02 13:27:09 +0200

    I dont really know
    No idea

---
+++ Animation/Assets/Char_Controller.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Char_Controller : MonoBehaviour
+{
+
+    float speed = 4;
+    float rotSpeed = 80;
+    float rot = 0f;
+    float gravity = 8;
+
+    Vector3 moveDir = Vector3.zero;
+
+    CharacterController controller;
+    Animator anim;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
+        anim.enabled = true;
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Q))
+        {
+            anim.enabled = false;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            anim.SetInteger("Condition", 1);
+            moveDir = new Vector3(0, 0, 1);
+            moveDir *= speed;
+            moveDir = transform.TransformDirection(moveDir);
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            anim.SetInteger("Condition", 0);
+            moveDir = new Vector3(0, 0, 0);
+        }
+
+
+
+
+        rot += Input.GetAxis("Horizontal") * rotSpeed * Time.deltaTime;
+        transform.eulerAngles = new Vector3(0, rot, 0);
+
+
+        moveDir.y -= gravity * Time.deltaTime;
+        controller.Move(moveDir * Time.deltaTime);
+    }
+}
